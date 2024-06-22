f01e495a8dfa7bcdde7afb89ba3b25d66d0fbdd3
Nerd <studytime023@gmail.com>
2024-06-22 11:39:37 +0300

    refractor (Application) : reconfigure the home page

--- application/urls.py
+++ application/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path,include
 from django.views.generic.base import TemplateView as home
 
 urlpatterns = [
-    path("",home.as_view(template_name="index.html"),name="home"),
+    path("",include("portfolio.urls")),
 ]
